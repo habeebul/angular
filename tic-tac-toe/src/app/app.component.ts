e8b447f19201fd0c244786d6f7ca7a28d3aa5fc0
habeebul@gmail.com <habeebul@gmail.com>
2019-09-25 20:43:03 +0530

    basic tic-tac-toe

--- tic-tac-toe/src/app/app.component.ts
+++ tic-tac-toe/src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'tic-tac-toe';
+  title = "tic-tac-toe";
+  squares = ["", "", "", "", "", "", "", "", ""];
+  xIsNext = true;
+  status = "Next player: X";
+  clickHandler(i) {
+    const squares = this.squares.slice();
+    if (this.calculateWinner(this.squares) || this.squares[i]) {
+      return "";
+    }
+    this.squares[i] = this.xIsNext ? "X" : "O";
+    this.xIsNext = !this.xIsNext;
+
+    const winner = this.calculateWinner(this.squares);
+    if (winner) {
+      this.status = "Winner : " + winner;
+    } else {
+      this.status = "Next player: " + (this.xIsNext ? "X" : "O");
+    }
+  }
+  calculateWinner(squares) {
+    let i = 0;
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
 }
